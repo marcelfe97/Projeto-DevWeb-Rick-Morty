fd960103a8ab1b17633177734e6a2801e5952736
Marcel Felix <marcelfelix1997@gmail.com>
2023-09-23 22:03:56 -0300

    teste javascrip API

--- js/login.js
+++ js/login.js
@@ -1,5 +1,22 @@
 'use strict'
 
+fetch('https://rickandmortyapi.com/api/episode')
+    .then((Response) => Response.json())
+.then((data) => Cards (data.results));
+
+function Cards(Stuff) {
+    const cardContainer = document.querySelector("#video")
+    console.log(Stuff)
+    Stuff.forEach(episode => {
+        cardContainer.innerHTML =
+        cardContainer.innerHTML +
+        `<div class="card">
+        <h2>${episone.name}<h2>
+        <video src=${episode.video} class="card-video-top>
+        </div>`
+    })
+}
+
 const loginContainer = document.getElementById('login-container')
 
 const moveOverlay = () => loginContainer.classList.toggle('move')
