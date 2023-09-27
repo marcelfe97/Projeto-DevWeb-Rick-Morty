46232fd7b07a67882d4d947555935c888983ec59
Marcel Felix <marcelfelix1997@gmail.com>
2023-09-27 20:52:52 -0300

    javascrip API

--- js/login.js
+++ js/login.js
@@ -1,14 +1,34 @@
 'use strict'
 
 fetch('https://rickandmortyapi.com/api/episode')
-    .then((Response) => Response.json())
-.then((data) => Cards (data.results));
+.then((Response) => Response.json())
+.then(episodes => {
+    console.log(episodes)
+});
+
+function renderEpisode(episode) {
+    episode.forEach(episode => {
+        const div = document.createElement('div');
+        const image = documment.createElement('img');
+        const name = document.createElement('h3')
+        const episode = document.createElement('h3')
+        div.classList = 'card'
+        image.classList = 'card-img'
+
+        image.src = episode.image
+        name.innerText = `Name: ${episode.name}`
+
+        div.appendChild(image)
+        div.appendChild(name)
+        cardsContainer.appendChild(div)
+    })
+}
 
 function (){
     var questions = [{
         questions: "O que o Morty queria do Presidente?",
         choices: ["Uma Selfie", "Um prêmio", "Uma camiseta", "Um autografo"]
-        correctAnswer: "Uma Selfie"
+        correctAnswer: "Uma Selfie",
     }]
 }
 
